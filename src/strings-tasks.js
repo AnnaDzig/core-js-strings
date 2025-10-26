54493743fc3657779be8faeccfc9bf591f8436a1
AnnaDzig <adzyhota@protonmail.com>
2025-10-26 20:10:02 +0100

    feat: solve extractNameFromTemplate func

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -405,8 +405,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const str = value.trim();
+  return str.slice(7, -1);
 }
 
 /**
